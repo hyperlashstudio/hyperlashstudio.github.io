1b1d81b407f6ef5ce1e8647a129b9f05017c3a49
hyperlashstudio <51031673+hyperlashstudio@users.noreply.github.com>
2019-05-26 11:18:53 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -63,9 +63,9 @@
       // else:
 
       images: [
-        "img/bgslides/1.jpg",
-        "img/bgslides/2.jpg",
-        "img/bgslides/3.jpg"
+        "3.jpg",
+        "2.jpg",
+        "1.jpg"
       ],
 
       onStart: function() {},
